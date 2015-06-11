64005fbc29791e62301d21d9a15272897d65acc4
Liz Coffey <ejcoffey@gmail.com>
2015-06-10 20:58:17 -0400

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+
+def update
+  @task.update(task_params)
+end
+
+
+def ajax
+end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.create(task_params)
+  end
+
+def destroy
+  @task.destroy
+end
+
+
+  private
+
+    def all_tasks
+      @tasks = Task.all
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:description, :deadline)
+    end
+end
